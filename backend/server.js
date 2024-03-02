89107be05c347c3ab91b739ad05f64d60d999578
saugat21 <saugatbaral122@gmail.com>
2024-03-02 21:12:51 +0545

    setting up mongodb database

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,14 @@ import express from "express";
 import dotenv from 'dotenv';
 import userRoutes from './routes/userRoutes.js'
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDb from "./config/db.js";
 
 //using dotenv variables
 dotenv.config();
 const port = process.env.PORT || 5000;
+
+// connecting database
+connectDb();
 
 
 const app = express();
