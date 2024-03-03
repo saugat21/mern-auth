5efedb60d91180f041b2ebb0568aed0cd66d4eff
saugat21 <saugatbaral122@gmail.com>
2024-03-03 22:34:03 +0545

    auth slice created

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
+import authReducer from './authSlice'
 
 const store = configureStore({
-    reducer: {},
+    reducer: {
+        auth: authReducer
+    },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
     devTools: true
 });
